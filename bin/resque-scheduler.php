5faca5e986a2d5172a0b17c3bc92730bf206758c
Tyler Sommer <sommertm@gmail.com>
2013-10-30 13:15:46 -0700

    Fixed autoloading in scripts when used from symfony's root

--- bin/resque-scheduler.php
+++ bin/resque-scheduler.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once __DIR__ . '/../vendor/autoload.php';
+(@include_once __DIR__ . '/../vendor/autoload.php') || @include_once __DIR__ . '/../../../../../../autoload.php';
 
 $REDIS_BACKEND = getenv('REDIS_BACKEND');
 if (!empty($REDIS_BACKEND)) {
